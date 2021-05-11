38f6c52911a63ddcb46bc800bfdfce0094963842
Leah King <dancingheart714@yahoo.com>
2021-05-11 14:33:46 -0400

    migration schema table done

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,16 @@
-// DO YOUR MAGIC
+//vin, make, model, mileage, transmission type, status of title
+exports.up = async function (knex) {
+  await knex.schema.table('cars', (table) => {
+    table.increments('car_id');
+    table.text('car_vin').unique.notNullable();
+    table.text('car_make').notNullable();
+    table.text('car_model').notNullable();
+    table.integer('car_mileage').notNullable();
+    table.text('transmission_type');
+    table.text('status_of_title');
+  });
+};
+
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('cars');
+};
